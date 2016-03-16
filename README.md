13f923b53168bcd15b1d6f5b3c1225c0e1ad5196
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2016-03-16 11:37:15 -0500

    Adding changes

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ npm install --save-dev gulp
 npm install --save-dev winston
 
 npm install --save-dev yargs
+
+npm install --save-dev valid-url
+
+npm install --save-dev gulp-webshot
+
+npm install --save-dev url-to-screenshot
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ var winston = require('winston');
 
 var argv = require('yargs').argv;
 
+var validUrl = require('valid-url');
+
+var screenshot = require('url-to-screenshot');
+
+var fs = require('fs');
+
 winston.add(winston.transports.File, {filename: 'custom-log.log'});
 //winston.remove(winston.transports.Console);
 
@@ -28,6 +34,24 @@ gulp.task(
 	}
 );
 
+/**
+ *
+ * @param url
+ * @param width
+ * @param height
+ */
+function createScreenshot(url, width, height, alias) {
+	screenshot(url).width(width).height(height).capture(
+		function(err, img) {
+			if (err) {
+				throw err;
+			}
+
+			fs.writeFileSync(__dirname + '/shots/' + alias + '.png', img);
+		}
+	);
+}
+
 gulp.task(
 	'test-log-input',
 	function() {
@@ -36,9 +60,23 @@ gulp.task(
 		var url = argv.url;
 
 		if (url === undefined) {
-			console.log("You must indicate the url");
+			winston.log('info', 'You must indicate the url');
 		} else {
-			console.log("url" + " = " + url);
+			winston.log('info', 'url' + ' = ' + url);
+
+			if (validUrl.isUri(url)){
+				winston.log('info', 'URL is valid');
+
+				var sizeForDesktop = {width: 1280, height: 1024};
+				var sizeForTablet = {width: 1024, height: 768};
+				var sizeForMobile = {width: 480, height: 320};
+
+				createScreenshot(url, sizeForDesktop.width, sizeForDesktop.height, "desktop");
+				createScreenshot(url, sizeForTablet.width, sizeForTablet.height, "tablet");
+				createScreenshot(url, sizeForMobile.width, sizeForMobile.height, "mobile");
+			} else {
+				winston.log('info', 'URL is not valid');
+			}
 		}
 	}
 );
